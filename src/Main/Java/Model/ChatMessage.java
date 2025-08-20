5024ec2b241347742fe8a941579bb36f212c0292
Sahil <sahildomale598@gmail.com>
2025-08-21 00:40:14 +0530

    Add files via upload

---
+++ src/Main/Java/Model/ChatMessage.java
@@ -0,0 +1,40 @@
+package comchatapplication.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+
+@Data
+public class ChatMessage {
+
+
+    private  Long id;
+    private String sender ;
+    private String content ;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
